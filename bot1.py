6cfa44b6971f124a6babc6a57de7351c233840aa
ninjadragon777 <olivarojasluis@gmail.com>
2025-02-05 10:20:42 -0600

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,29 +1,86 @@
 import discord
+from discord.ext import commands
+import random
 from botlogic import gen_pass
 
-# La variable intents almacena los privilegios del bot
+description = '''Un bot de ejemplo para mostrar la extensión discord.ext.commands.
+module.
+
+Aquí se muestran varios comandos de utilidad.'''
+
 intents = discord.Intents.default()
-# Activar el privilegio de lectura de mensajes
+intents.members = True
 intents.message_content = True
-# Crear un bot en la variable cliente y transferirle los privilegios
+
+bot = commands.Bot(command_prefix='¿', description=description, intents=intents)
 client = discord.Client(intents=intents)
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f'Hemos iniciado sesión como {client.user}')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """suma dos numeros."""
+    await ctx.send(left + right)
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """lanza un dado en formato NdN."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('el formato tiene que ser NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='para cuando quieras decidir de otra manera')
+async def choose(ctx, *choices: str):
+    """elige entre varias opciones."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repitiendo...'):
+    """reoite un mensaje multiples veces."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """dice cuendo se une un miembro."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.group()
+async def cool(ctx):
+    """dice si un miembro es cool.
+
+    realmente solo verifica si se está invocando un subcomando.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} no es cool')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """el bot es cool?"""
+    await ctx.send('Si, el bot es cool.')
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_message(msg):
+    if msg.author == client.user:
         return
-    if message.content.startswith('hola'):
-        await message.channel.send("que rollo plebes")
-    elif message.content.startswith('chao'):
-        await message.channel.send("hasta luego chao")
-    elif message.content.startswith('dame una contraseña'):
-        await message.channel.send(gen_pass(10))
-    else:
-        await message.channel.send(message.content)
+    if msg.content.startswith('hola'):
+        await msg.channel.send("que rollo plebes")
 
 
 
